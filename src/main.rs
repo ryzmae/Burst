ff0131692f45d72a3b7353e024122d1a87d77b8e
ryzm <Juwennagpal6@gmail.com>
2024-09-26 20:55:07 +0200

    add MIT License

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,8 @@ pub mod store;
 pub mod memory;
 pub mod error;
 pub mod prelude;
+pub mod server;
 
-// Testing only
-fn main() {
-    let test1 = store::Store::new();
-
-    let _ = test1.set_data("Test".to_string());
-
-    println!("{:?}", test1.get_data());
+fn main () {
+    println!("Hello, world!");
 }
---
+++ src/server.rs
@@ -0,0 +1,25 @@
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+pub fn run(address: &str) {
+    let listner = TcpListener::bind(address).unwrap();
+
+    for stream in listner.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+pub fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {http_request:#?}");
+}
