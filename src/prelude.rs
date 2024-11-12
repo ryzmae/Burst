affee6214f79cbe2dcc7bb1051cfd7a57d6852c9
ryzm <114670723+ryzmae@users.noreply.github.com>
2024-11-12 16:06:37 +0100

    refactor: Simplify main function return type and clean up imports

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,6 +2,5 @@ pub use crate::error::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
+// With this prelude module, we can now use the Result type alias in other modules without having to import it.
 pub struct W<T>(pub T);
-
-pub use std::format as f;
